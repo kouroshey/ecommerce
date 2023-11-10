4aa0cbb1b95f232acbfd70eba35035ab9312783e
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-10 23:29:38 +0330

    add colors and fontsize

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'pink': '#EDA3B5',
+      'white': '#FFFFF',
+      'black': '#1B2437',
+      'gray': {
+        1: '#1B2437',
+        2: '#EFEFF4',
+        3: '#E5E5EA',
+        4: '#D1D1D6',
+        5: '#C7C7CC',
+        6: '#8E8E93',
+        7: '#48484A',
+      }
+    },
+    extend: {
+      fontSize: {
+        'h1': '48px',
+        'h2': '34px',
+        'h3': '24px',
+        'h4': '20px',
+        'sub': {
+          1: '16px',
+          2: '14px',
+        },
+        'body': '14px',
+        'caption': '12px',
+        'overline': '10px'
+      }
+    },
   },
   plugins: [],
 }
