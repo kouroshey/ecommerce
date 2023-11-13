f3e71fd5b2cfe046fd90d4e4478c0316a61a45ff
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-13 13:39:35 +0330

    design Navbar comp

--- src/components/partials/header/content/Navbar.jsx
+++ src/components/partials/header/content/Navbar.jsx
@@ -1,12 +1,33 @@
 import { NavLink } from "react-router-dom"
 
-const Navbar = ({width, breakPoints}) => {
+const Navbar = ({ width, breakPoints }) => {
+    const navItems = [
+        {
+            id: 1, title: 'خانه', path: '/',
+        },
+        {
+            id: 2, title: 'فروشگاه', path: '/shop',
+        },
+        {
+            id: 3, title: 'بلاگ', path: '/blog',
+        },
+        {
+            id: 4, title: 'ارتباط با ما', path: '/contact',
+        }
+        
+    ]
     return (
         <ul className={`${width < breakPoints.lg ? "gap-6" : 'gap-12'} flex w-full justify-center text-h4`}>
-            <li><NavLink to='/'>خانه</NavLink></li>
-            <li><NavLink to='shop'>فروشگاه</NavLink></li>
-            <li><NavLink to='#'>بلاگ</NavLink></li>
-            <li><NavLink to='#'>ارتباط با ما</NavLink></li>
+            {navItems.map(item => (
+                <li>
+                    <NavLink
+                        className={({ isActive, isPending }) => isActive? 'after:w-full relative after:absolute after:h-[1px] after:bg-pink after:-bottom-2 after:right-0': ''}
+                        to={item.path}
+                    >
+                        {item.title}
+                    </NavLink>
+                </li>
+            ))}
         </ul>
     )
 }
