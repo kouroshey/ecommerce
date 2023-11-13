7f7e846ff1a69f681001ef48142f95f0c09d6bf3
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-13 13:38:46 +0330

    create and add error element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Layout from './layout/Layout'
 import Home from './pages/home/Home'
 import Shop from './pages/shop/Shop'
+import Error from './pages/404'
 import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <main>
       <Routes>
-        <Route path='/' element= {<Layout />}>
+        <Route
+          path='/'
+          element={<Layout />}
+          errorElement={<Error />}
+        >
           <Route index element={<Home />} />
           <Route path='shop' element={<Shop />} />
         </Route>
