d5acf49975c7fbc53f5785ee7b79a404fdd41520
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-14 17:56:06 +0330

    make Cards responsive

--- src/pages/home/content/Cards.jsx
+++ src/pages/home/content/Cards.jsx
@@ -1,46 +1,58 @@
 import Card from "../../../components/ui/Card"
+import useWidth from "../../../hooks/useWidth"
 
 const Cards = () => {
     const cardContentDetails = [
-        {id: 1, text: 'مزون زنانه', cardText: 'مزون زنانه', desc: 'بیزنس بیزنس'},
-        {id: 2, text: 'مزون دخترانه', cardText: 'مزون دخترانه', desc: 'بیزنس بیزنس'},
-        {id: 3, text: 'مزون مردانه', cardText: 'مزون مردانه', desc: 'بیزنس بیزنس'},
-        {id: 4, text: 'مزون پسرانه', cardText: 'مزون پسرانه', desc: 'بیزنس بیزنس'},
-      ]
-  return (
-    <section className="flex gap-4 flex-row-reverse">
-        <div className="flex-1">
-          <Card
-            cardText= {cardContentDetails[0].cardText}
-            desc= {cardContentDetails[0].desc}
-            text= {cardContentDetails[0].text}
-          />
-        </div>
-        <div className="flex-col flex flex-1 h-full gap-4">
-          <div className="flex gap-4">
-            <Card
-              cardText={cardContentDetails[1].cardText}
-              desc={cardContentDetails[1].desc}
-              text={cardContentDetails[1].text}
-            />
-            <Card
-              cardText={cardContentDetails[2].cardText}
-              desc={cardContentDetails[2].desc}
-              text={cardContentDetails[2].text}
-            />
-          </div>
-          <div className="">
-            <Card
-              cardText={cardContentDetails[0].cardText}
-              desc={cardContentDetails[0].desc}
-              text={cardContentDetails[0].text}
-              maxHeight='h-[12.4rem]'
-              textPosition='left'
-            />
-          </div>
-        </div>
-      </section>
-  )
+        { id: 1, text: 'مزون زنانه', cardText: 'مزون زنانه', desc: 'بیزنس بیزنس' },
+        { id: 2, text: 'مزون دخترانه', cardText: 'مزون دخترانه', desc: 'بیزنس بیزنس' },
+        { id: 3, text: 'مزون مردانه', cardText: 'مزون مردانه', desc: 'بیزنس بیزنس' },
+        { id: 4, text: 'مزون پسرانه', cardText: 'مزون پسرانه', desc: 'بیزنس بیزنس' },
+    ]
+    const { width, breakPoints } = useWidth()
+    return (
+        <section className=" gap-4 grid md:grid-cols-2 xl:flex-row-reverse xl:flex">
+            <div className="xl:flex-1 flex flex-col gap-4">
+                <Card
+                    cardText={cardContentDetails[0].cardText}
+                    desc={cardContentDetails[0].desc}
+                    text={cardContentDetails[0].text}
+                />
+                {width < breakPoints.xl &&
+                        <Card
+                        cardText={cardContentDetails[0].cardText}
+                        desc={cardContentDetails[0].desc}
+                        text={cardContentDetails[0].text}
+                        maxHeight={width > breakPoints.xl && 'h-[12.4rem]'}
+                        textPosition={width > breakPoints.xl && 'left'}
+                    />}
+            </div>
+            <div className="flex-col flex xl:flex-1 h-full gap-4">
+                <div className="flex gap-4 flex-col xl:flex-row">
+                    <Card
+                        cardText={cardContentDetails[1].cardText}
+                        desc={cardContentDetails[1].desc}
+                        text={cardContentDetails[1].text}
+                    />
+                    <Card
+                        cardText={cardContentDetails[2].cardText}
+                        desc={cardContentDetails[2].desc}
+                        text={cardContentDetails[2].text}
+                    />
+                </div>
+                <div className="">
+                    {width > breakPoints.xl &&
+                        <Card
+                        cardText={cardContentDetails[0].cardText}
+                        desc={cardContentDetails[0].desc}
+                        text={cardContentDetails[0].text}
+                        maxHeight={width > breakPoints.xl && 'h-[12.4rem]'}
+                        textPosition={width > breakPoints.xl && 'left'}
+                    />}
+
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Cards
