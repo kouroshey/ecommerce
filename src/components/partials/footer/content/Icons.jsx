32870c7b3e3f32a3d4eed0c34275ad79d57611ed
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-15 19:14:25 +0330

    create and design footer icons comp

---
+++ src/components/partials/footer/content/Icons.jsx
@@ -0,0 +1,11 @@
+import Socials from '../../header/content/Socials.jsx'
+const Icons = () => {
+  return (
+      <div className='w-full py-6 bg-gray-3 flex justify-between items-center px-8 sm:px-12 md:px-12 lg:px-24 '>
+          <Socials />
+          <span>تمامی حقوق محفوظ است ©</span>
+    </div>
+  )
+}
+
+export default Icons
