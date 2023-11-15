9520df21b8f8dfe80687c7d8ee09aca7df5094dd
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-15 19:14:01 +0330

    create and desing footer comp

--- src/components/partials/footer/index.jsx
+++ src/components/partials/footer/index.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import Navbar from "../header/content/Navbar"
+import Info from "./content/Info"
+import Links from "./content/Links"
+import MoreInfo from "./content/MoreInfo"
+
+import useWidth from "../../../hooks/useWidth"
+import Icons from "./content/Icons"
 
 const Footer = () => {
+  const { width, breakPoints } = useWidth()
+
   return (
-    <div>Footer</div>
+    <footer className="w-full">
+      {width > breakPoints.sm &&
+        <section className="w-full bg-white py-4">
+          <Navbar width={width} breakPoints={breakPoints} />
+        </section>
+      }
+      <section className="flex px-8 sm:px-12 md:px-12 lg:px-24 py-8 gap-6">
+        <section className="flex-1 flex gap-36">
+          <Links />
+          <MoreInfo />
+        </section>
+        {width > breakPoints.md &&
+          <section className="flex justify-end flex-1">
+            <Info />
+          </section>
+        }
+      </section>
+      <section>
+        <Icons />
+      </section>
+    </footer>
   )
 }
 
