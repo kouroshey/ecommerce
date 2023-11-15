c4fc888c152f9fbaaef527a4050335440a14a1ec
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-15 19:14:37 +0330

    create and design info comp

---
+++ src/components/partials/footer/content/Info.jsx
@@ -0,0 +1,16 @@
+import Logo from '../../header/content/Logo'
+
+const Info = () => {
+    return (
+        <div className='flex flex-col gap-4'>
+            <div className='flex justify-end'>
+            <Logo />
+            </div>
+            <div className='text-justify text-gray-4'>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد .
+            </div>
+        </div>
+    )
+}
+
+export default Info
