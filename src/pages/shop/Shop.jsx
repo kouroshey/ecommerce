e42fffa1af77b8ed16d01b71f50645d28604475a
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-17 12:25:29 +0330

    add collapse option for filters

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,13 +1,17 @@
 import Filters from "./content/Filters"
+import ProductsList from "./content/ProductsList"
 
 const Shop = () => {
   return (
-    <>
+    <div className="flex flex-col gap-8">
       <section className="py-4">
       <Filters />
       </section>
-      <span>shop</span>
-    </>
+      <h2 className="text-h3 after:w-full after:absolute relative after:bg-pink after:right-0 after:-bottom-1 after:h-1 after:rounded-md w-max">محصولات</h2>
+      <section>
+        <ProductsList />
+      </section>
+    </div>
   )
 }
 
--- src/pages/shop/content/Filters.jsx
+++ src/pages/shop/content/Filters.jsx
@@ -1,24 +1,48 @@
+import { useState } from "react"
 import DropDown from "../../../components/ui/DropDown"
+import { IoChevronDown } from "react-icons/io5"
+
 
 const Filter = () => {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const collapseHandle = () => {
+    setCollapsed(prev => prev = !prev)
+  }
   const options = {
-    showOptions : [
-      {id: '1', title: 'جدید ترین', isDefault: true},
-      {id: '2', title: 'محبوب ترین', isDefault: false},
-      {id: '3', title: 'ارزان ترین', isDefault: false},
-      {id: '4', title: 'گران ترین', isDefault: false},
+    showOptions: [
+      { id: '1', title: 'جدید ترین', isDefault: true },
+      { id: '2', title: 'محبوب ترین', isDefault: false },
+      { id: '3', title: 'ارزان ترین', isDefault: false },
+      { id: '4', title: 'گران ترین', isDefault: false },
     ],
     sizeOptions: [
-      {id: '1', title: 'XS (کوچکترین)', isDefault: false},
-      {id: '2', title: 'SM (کوچک)', isDefault: false},
-      {id: '3', title: 'M (متوسط)', isDefault: true},
-      {id: '4', title: 'L (بزرگ)', isDefault: false},
+      { id: '1', title: 'XS (کوچکترین)', isDefault: false },
+      { id: '2', title: 'SM (کوچک)', isDefault: false },
+      { id: '3', title: 'M (متوسط)', isDefault: true },
+      { id: '4', title: 'L (بزرگ)', isDefault: false },
     ]
   }
   return (
-      <div className="flex gap-12">
-      <DropDown items={options.showOptions} label='نمایش بر اساس' />
-      <DropDown items={options.sizeOptions} label='انتخاب سایز' />
+    <div className="flex gap-12">
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4 items-center">
+          {collapsed ? <p>نمایش دادن فیلتر ها</p> : <p>پنهان کردن فیلتر ها</p>}
+          <button
+            className="bg-pink rounded-md p-1 text-white"
+            onClick={collapseHandle}
+          >
+            <IoChevronDown className={`transition-all ${collapsed ? 'rotate-0' : 'rotate-180'}`} />
+          </button>
+        </div>
+        {!collapsed ?
+          <div className="flex items-center gap-4">
+            <DropDown items={options.showOptions} label='نمایش بر اساس' />
+            <DropDown items={options.sizeOptions} label='انتخاب سایز' />
+          </div> : ''
+        }
+      </div>
+
     </div>
   )
 }
