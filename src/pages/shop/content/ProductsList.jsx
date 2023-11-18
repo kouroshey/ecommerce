90b04a8c8e4531c0a8beb5a390cadf3ad86d0374
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-18 09:22:18 +0330

    add react pagination to project

--- src/pages/shop/content/ProductsList.jsx
+++ src/pages/shop/content/ProductsList.jsx
@@ -1,11 +1,16 @@
 import ProductCard from './ProductCard'
+import PaginationButtons from '../../../components/ui/PaginationButtons'
 
-const ProductsList = ({products}) => {
+const ProductsList = ({ products }) => {
+  
   return (
     <div>
-      {products.map(product => (
-        <ProductCard key={product.id} {...product} />
-      ))}
+      <div className='w-full grid grid-cols-4 gap-8 '>
+        {products.map(product => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+      <PaginationButtons />
     </div>
   )
 }
