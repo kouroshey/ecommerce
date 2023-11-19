a999cee4707d7e61c1a8e906f17433b2560e170b
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-19 10:29:23 +0330

    add functionality to modal

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import { useRef } from "react";
 
-const Modal = ({children}) => {
-  return (
-      <div className='absolute w-full h-screen z-50 bg-black/50'>
-          <div className='flex w-full h-full items-center justify-center'>
-          <div className='bg-white rounded-md max-w-[80%] min-w-[30%] absolute'>
-          {children}
-          </div>
-    </div>
-      </div>
-  )
+const Modal = ({ children, modalCloseHandle }) => {
+    const mainElementRef = useRef(null)
+
+    const closeModalHandle = event => {
+        if (!mainElementRef.current.contains(event.target)) {
+            modalCloseHandle()
+        }
+    }
+    return (
+        <div className='fixed w-full h-screen z-40 left-0 bg-black/50' onClick={closeModalHandle}>
+            <div className='flex w-full h-full items-center justify-center'>
+                <div className='bg-white rounded-lg max-w-[80%] min-w-[30%] absolute z-50' ref={mainElementRef}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Modal
