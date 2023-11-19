43166b3c9726b056279fb9598d093a98e4c28432
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-19 12:02:55 +0330

    craete and design close btn

---
+++ src/components/ui/CloseBtn.jsx
@@ -0,0 +1,14 @@
+import { IoCloseSharp } from "react-icons/io5"
+
+const CloseBtn = ({ closeFunction }) => {
+    const closeHandle = () => {
+        closeFunction()
+    }
+    return (
+        <span className="text-h3 hover:text-pink hover:scale-105" onClick={closeHandle} >
+            <IoCloseSharp />
+        </span>
+    )
+}
+
+export default CloseBtn
