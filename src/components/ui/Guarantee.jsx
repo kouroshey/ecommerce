7394eeb08a0f82d63209e7c2e0753a674f8bedfe
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-19 10:29:05 +0330

    craete and design

---
+++ src/components/ui/Guarantee.jsx
@@ -0,0 +1,12 @@
+
+
+const Guarantee = () => {
+  return (
+      <div>
+          <span><img src="" alt="" srcset="" /></span>
+          <span className='before:content-["\F04D\00FE0E"] before:flex before:w-10 before:h-10 before:text-gray-6 before:text-h2'>آماده ارسال</span>
+    </div>
+  )
+}
+
+export default Guarantee
