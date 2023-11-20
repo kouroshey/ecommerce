5d12c5fed27ba88fe4ddc997734b1125c46dd82c
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-20 14:47:49 +0330

    craete login and register route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Home from './pages/home/Home'
 import Shop from './pages/shop/Shop'
 import Cart from './pages/cart/Cart'
 import Error from './pages/404'
+import Login from './pages/login/Login'
 import { Route, Routes } from 'react-router-dom'
+import Register from './pages/login/content/login-reg'
 
 
 const App = () => {
@@ -19,6 +21,8 @@ const App = () => {
           <Route path='shop' element={<Shop />} />
           <Route path='cart' element={<Cart />} />
         </Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
       </Routes>
     </main>
   )
