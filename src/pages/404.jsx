f777356a5ff447081658fecbd85a4aa420e97b34
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-20 17:49:01 +0330

    design 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 
-const Error = () => {
+function Error404() {
   return (
-    <div>Error: Page Not Found</div>
-  )
+    <main className="flex justify-center items-center w-screen h-screen flex-col gap-6">
+      <span className="text-pink text-[10rem]">404</span>
+      <span className="">متاسفانه صفحه مورد نظر شما یافت نشد!</span>
+      <Link to='/' className="border rounded-md px-4 py-2 border-pink hover:bg-pink hover:text-white transition">بازگشت به صفحه اصلی</Link>
+    </main>
+  );
 }
 
-export default Error
+export default Error404;
