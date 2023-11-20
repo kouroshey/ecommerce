2541a014e531734bcb8b10a462611a48ab0cd617
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-20 17:49:27 +0330

    add error boundary

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import HomeSlider from "./content/HomeSlider"
 import ProductSlider from "./content/ProductSlider"
 
 import { useSelector, useDispatch } from "react-redux"
-import { selectAllProducts, fetchProducts } from "../shop/content/store"
+import { selectAllProducts, fetchProducts, getProductsErr, getProductsStatus } from "../shop/content/store"
 
 const Home = () => {
   const slides = [
@@ -19,14 +19,26 @@ const Home = () => {
     { id: 9, img: '../public/images/suit.png', name: 'suit' },
     { id: 8, img: '../public/images/dress2.png', name: 'dress2' }
   ]
-
+  const status = useSelector(getProductsStatus)
+  const err = useSelector(getProductsErr)
   const allProducts = useSelector(selectAllProducts)
-  const popularProducts = allProducts.filter(product => product.popular === true)
+  const popularProducts = allProducts?.filter(product => product.popular === true)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchProducts())
 
   }, [])
+
+  let content;
+  if (status === 'idle') {
+    dispatch(fetchProducts())
+  } else if (status === 'pending') {
+    content = <p className="text-favorite py-4">لطفا منتظر لود شدن محصولات بمانید...</p>
+  } else if (status === 'succeeded') {
+    content = <ProductSlider products={popularProducts} />
+  } else if (status === 'failed') {
+    content = <p className="text-favorite py-4">خطا در گرفتن اطلاعات از سرور. لطفا اینترنت خود را چک کنید و صفحه را رفرش کنید</p>
+  }
 
   return (
     <main className="pt-6 flex flex-col gap-12">
@@ -38,7 +50,7 @@ const Home = () => {
       {/* products slider */}
       <section className="w-full rounded-md">
         <h2 className="text-h3 w-max border-b-2 border-b-pink pb-2">محصولات محبوب</h2>
-        <ProductSlider products={popularProducts} />
+        {content}
       </section>
     </main>
   )
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,17 +1,21 @@
 import Filters from "./content/Filters"
 import ProductsList from "./content/ProductsList"
 
-import { useSelector, useDispatch } from "react-redux"
-import { selectAllProducts, fetchProducts } from "./content/store"
-import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { selectAllProducts, getProductsStatus } from "./content/store"
 
 const Shop = () => {
   const products = useSelector(selectAllProducts)
-  const dispatch = useDispatch()
-  useEffect(() => {
-      dispatch(fetchProducts())
-      
-  }, [])
+  const status = useSelector(getProductsStatus)
+
+  let content;
+  if (status === 'pending') {
+    content = <p className="text-favorite py-4">لطفا منتظر لود شدن محصولات بمانید...</p>
+  } else if (status === 'succeeded') {
+    content = <ProductsList products={products}/>
+  } else if (status === 'failed') {
+    content = <p className="text-favorite py-4">خطا در گرفتن اطلاعات از سرور. لطفااینترنت خود را چک کنید و صفحه را رفرش کنید</p>
+  }
 
   return (
     <div className="flex flex-col gap-8">
@@ -20,7 +24,7 @@ const Shop = () => {
       </section>
       <h2 className="text-h3 after:w-full after:absolute relative after:bg-pink after:right-0 after:-bottom-1 after:h-[2px] after:rounded-md w-max">محصولات</h2>
       <section>
-        <ProductsList products={products}/>
+        {content}
       </section>
     </div>
   )
