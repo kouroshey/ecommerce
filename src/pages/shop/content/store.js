2aa5e0056e7036ef6f481928dc691b2acd5a32b0
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-20 17:50:29 +0330

    implement products logic

--- src/pages/shop/content/store.js
+++ src/pages/shop/content/store.js
@@ -1,55 +1,53 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import {supabase} from '../../../config/supabaseClient'
+import { supabase } from '../../../config/supabaseClient'
 
 const initialState = {
     products: [],
-    status: null,
+    status: 'idle', // idle, pending, succeded, 'failed'
+    error: null,
     isProductModalShow: false,
-    modalProduct: {}
+    modalProduct: {},
 }
 
 export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
-    try {
-        let { data, error } = await supabase
-        .from('products')
-            .select('*')
-        return data
-    } catch (err) {
-        console.log(err);
-    }
+    let { data, error } = await supabase
+    .from('products')
+        .select('*')
+    if (data) {return data}
 })
 
 const productsSlice = createSlice({
     name: 'products',
     initialState,
     reducers: {
-        fetchProducts(state, action) {
-
-        },
         productModalShowHandler(state, action) {
             state.isProductModalShow = !state.isProductModalShow
             state.modalProduct = state.products.find(product => product.id === action.payload)
-
         }
-        
     },
     extraReducers(builder) {
         builder
             .addCase(fetchProducts.pending, (state, action) => {
-            state.status = 'loading'
+                state.status = 'loading'
+                console.log(state.status);
             })
             .addCase(fetchProducts.fulfilled, (state, action) => {
-                state.status = 'succeeded'
-                state.products = action.payload
+                if (action.payload) {
+                    state.products = action.payload
+                    state.status = 'succeeded'
+                    console.log(action.payload);
+                } else {
+                    state.status = 'failed'
+                    console.log('failed');
+                }
             })
-            .addCase(fetchProducts.rejected, (state, action) => {
-            console.log( 'failed' ,action.payload);
-        })
     }
 })
 
 export const selectAllProducts = (state => state.products.products)
 export const getModalShow = (state) => state.products.isProductModalShow
+export const getProductsErr = (state) => state.products.error
+export const getProductsStatus = (state) => state.products.status
 export const getModalProduct = (state) => state.products.modalProduct
 
 // actions
