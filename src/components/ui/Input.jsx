c1a080a07b5041634e85369350a93ababedd2ed1
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-20 14:49:37 +0330

    create an design customized input elem

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Input = ({
+    inputId,
+    icon,
+    label,
+    type,
+    value,
+    placeholder,
+    labelStyel = 'text-body',
+    defaultClass = 'border border-gray-3 rounded-md px-2 py-1 text-gray-7 focus:border-pink w-full outline-none'
+}) => {
+    return (
+        <div className='flex flex-col gap-4 w-full relative'>
+            <label
+                htmlFor={inputId}
+                className={`${labelStyel}`}
+            >{label}</label>
+            {icon &&
+                <span className='absolute right-2 bottom-2'>
+                    {icon}
+                </span>}
+            <input
+                dir='ltr'
+                type={type}
+                value={value}
+                placeholder={placeholder}
+                className={`${defaultClass}
+                
+              `}
+            />
+        </div>
+    )
+}
+
+export default Input
