fc9e5bc3248d5a01e1d66c17b31739b8b8f8b49b
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-20 14:49:58 +0330

    design Login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useState } from "react"
+import LoginImage from "./content/LoginImage"
+import Auth from "./content/login-auth"
+import Register from "./content/login-reg"
 
 const Login = () => {
+  const [isRegistered, setIsRegistered] = useState(true)
   return (
-    <div>Login</div>
+    <main className="h-screen">
+      <div className="bg-lightBg w-full h-full flex justify-center items-center">
+        {/* form */}
+          {isRegistered ? <Auth /> : <Register />}
+      </div>
+    </main>
   )
 }
 
