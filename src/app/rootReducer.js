0cf16f66c6661f25a62021e7ebed90a7ad3bec88
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-21 13:49:02 +0330

    add cart slice

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,9 +1,11 @@
 import products from '../pages/shop/content/store'
 import layout from './layout'
+import cart from '../pages/cart/content/store'
 
 const rootReducer = {
     products,
-    layout
+    layout,
+    cart
 }
 
 export default rootReducer
