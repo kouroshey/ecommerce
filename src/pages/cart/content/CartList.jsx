620ec4b5545a06c92cc9a5977b7be78adf1226da
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-21 13:50:15 +0330

    receive items from store

--- src/pages/cart/content/CartList.jsx
+++ src/pages/cart/content/CartList.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux"
 import CartItem from "./CartItem"
+import { getAllCartItems } from "./store"
 
-const CartList = ({items}) => {
+const CartList = () => {
+  const items = useSelector(getAllCartItems)
+  
   return (
       <section className="p-4 flex flex-col gap-6">
-          {items.map((item, index) => (
+          {items?.map((item, index) => (
               <CartItem border={index !== items.length - 1 ? 'border-b border-gray-4 border-dashed pb-6' : ''} {...item} />
           ))}
     </section>
