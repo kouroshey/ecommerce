53a5c2a9e72d1cd2718deeb8b89c973db7d781be
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-21 19:31:42 +0330

    handling errors

--- src/pages/cart/content/CartList.jsx
+++ src/pages/cart/content/CartList.jsx
@@ -6,10 +6,10 @@ const CartList = () => {
   const items = useSelector(getAllCartItems)
   
   return (
-      <section className="p-4 flex flex-col gap-6">
-          {items?.map((item, index) => (
+      <section className="p-4 flex flex-col gap-6 bg-white">
+          {items.length > 0 ? items.map((item, index) => (
               <CartItem border={index !== items.length - 1 ? 'border-b border-gray-4 border-dashed pb-6' : ''} {...item} />
-          ))}
+          )) : <span>سبد خرید شما خالی است!</span>}
     </section>
   )
 }
