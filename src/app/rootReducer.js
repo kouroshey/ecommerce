d4130a0d0a5bbe286a2903c007b988580e417223
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-21 19:32:01 +0330

    create and add login slice

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,11 +1,13 @@
 import products from '../pages/shop/content/store'
 import layout from './layout'
 import cart from '../pages/cart/content/store'
+import login from '../pages/login/content/store'
 
 const rootReducer = {
     products,
     layout,
-    cart
+    cart,
+    login
 }
 
 export default rootReducer
