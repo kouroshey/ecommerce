d64ef60aac0bad87fb99573419c58fffdec7a1d8
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-24 22:31:41 +0330

    add icons and an object for items and icons

--- src/components/partials/header/content/MobileNav.jsx
+++ src/components/partials/header/content/MobileNav.jsx
@@ -1,39 +1,54 @@
 import { NavLink } from "react-router-dom"
-import { IoCloseSharp } from 'react-icons/io5'
 import Socials from './Socials'
 import CloseBtn from "../../../ui/CloseBtn"
+import { IoHomeOutline, IoCartSharp, IoPersonOutline, IoCartOutline, IoNewspaperOutline, IoPeopleOutline } from "react-icons/io5";
 
 const MobileNav = ({ collapsed, showMenuHandler }) => {
     const navbarCloseHandle = () => {
         showMenuHandler()
     }
+
+    const menuItems = [
+        {
+            id: 1, title: 'خانه', icon: <IoHomeOutline />, path: '/',
+        },
+        {
+            id: 2, title: 'فروشگاه', icon: <IoCartSharp />, path: '/shop'
+        },
+        {
+            id: 3, title: 'پروفایل', icon: <IoPersonOutline />, path: '/register',
+        },
+        {
+            id: 4, title: 'سبد خرید', icon: <IoCartOutline />, path: '/cart',
+        },
+        {
+            id: 5, title: 'بلاگ', icon: <IoNewspaperOutline />, path: '#',
+        },
+        {
+            id: 6, title: 'ارتباط با ما', icon: <IoPeopleOutline />, path: '#',
+        },
+    ]
     return (
         <ul
             className={`
             ${collapsed ? 'translate-x-96' : 'translate-x-0'} 
-            absolute right-0 top-0 text-gray-7 transition duration-300 z-50 w-1/2 rounded-md h-screen bg-white pt-4 px-4 gap-4 flex flex-col`}
+            absolute right-0 top-0 text-gray-7 transition duration-300 z-50 w-1/2 rounded-md h-screen bg-white pt-4 gap-4 flex flex-col`}
         >
             <li
-                className="after:w-full relative after:absolute after:h-[1px] after:bg-pink after:-bottom-2 after:right-0"
+                className="relative pb-2 border-b border-pink "
             >
-                <span className="absolute left-0">
+                <span className="absolute left-2">
                     <CloseBtn closeFunction={navbarCloseHandle} />
                 </span>
-                <span>منو</span>
+                <span className="pr-2">منو</span>
             </li>
-            <li className="w-full hover:text-pink">
-                <NavLink to='/'>خانه</NavLink>
-            </li>
-            <li className="w-full hover:text-pink">
-                <NavLink to='shop'>فروشگاه</NavLink>
-            </li>
-            <li className="w-full hover:text-pink">
-                <NavLink to='#'>بلاگ</NavLink>
-            </li>
-            <li className="w-full hover:text-pink">
-                <NavLink to='#'>ارتباط با ما</NavLink>
-            </li>
-            <li className="flex justify-center w-full mt-16">
+            {menuItems.map(item => (
+                <li key={item.id} className="w-full items-center flex pr-2 gap-4 hover:text-pink">
+                    <span className="text-h4">{item.icon}</span>
+                    <NavLink to={item.path}>{item.title}</NavLink>
+                </li>
+            ))}
+            <li className="w-full absolute bottom-2 flex justify-center">
                 <Socials />
             </li>
         </ul>
