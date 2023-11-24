025d7054f82d1e352e0f0c77dc0d3db98d3a413d
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-24 22:35:30 +0330

    add toast on actions

--- src/pages/cart/content/store.js
+++ src/pages/cart/content/store.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
     items: [],
@@ -25,8 +26,8 @@ const cartSlice = createSlice({
                         itemTotalPrice: itemPrice
                     }
                 )
+                toast.success('آیتم به سبد خرید اضاف شد')
             }
-            console.log(state.items);
         },
         removeFromCart(state, action) {
             const existsItem = state.items.find(item => item.id === action.payload.id)
@@ -36,12 +37,14 @@ const cartSlice = createSlice({
                 existsItem.itemTotalPrice = existsItem.price * existsItem.amount
             } else if (existsItem.amount === 1) {
                 state.items = state.items.filter(item => item.id !== existsItem.id)
+                toast.success('آیتم از سبد خرید حذف شد')
             }
         },
         clearFromCart(state, action) {
             const existsItem = state.items.find(item => item.id === action.payload.id)
             state.mainPrice = state.mainPrice - (existsItem.price * existsItem.amount)
             state.items = state.items.filter(item => item.id !== existsItem.id)
+            toast.success('آیتم از سبد خرید حذف شد')
         }
     }
 })
@@ -52,6 +55,6 @@ export const getCartMainPrice = (state) => state.cart.mainPrice
 export const getDeliveryPrice = (state) => state.cart.delivery
 
 // actions
-export const {addToCart, removeFromCart, clearFromCart} = cartSlice.actions
+export const { addToCart, removeFromCart, clearFromCart } = cartSlice.actions
 
 export default cartSlice.reducer
