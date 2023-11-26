630d26ae8c17c101dc79ea6b2a3577e53a7e5828
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 22:29:04 +0330

    create and desing CSpinner

---
+++ src/components/ui/CSpinner.jsx
@@ -0,0 +1,11 @@
+import { BeatLoader } from 'react-spinners'
+
+const CSpinner = () => {
+    return (
+        <div className='w-screen h-screen flex justify-center items-center'>
+            <BeatLoader color='#EDA3B5' />
+        </div>
+    )
+}
+
+export default CSpinner
