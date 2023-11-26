34c7f7b2d5788703e82491ec880c6173edbb99ac
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 11:57:10 +0330

    conditional rendering

--- src/components/partials/footer/content/Icons.jsx
+++ src/components/partials/footer/content/Icons.jsx
@@ -1,9 +1,9 @@
 import Socials from '../../header/content/Socials.jsx'
 const Icons = () => {
   return (
-      <div className='w-full py-6 bg-gray-3 flex justify-between items-center px-8 sm:px-12 md:px-12 lg:px-24 '>
-          <Socials />
-          <span>تمامی حقوق محفوظ است ©</span>
+    <div className='w-full py-6 flex justify-between items-center px-8 sm:px-12 md:px-12 lg:px-24 '>
+      <Socials />
+      <span>تمامی حقوق محفوظ است ©</span>
     </div>
   )
 }
