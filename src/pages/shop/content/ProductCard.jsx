5711e934a76ce191d7d01ecd71732b578f84cd1d
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 11:53:40 +0330

    delet useless section

--- src/pages/shop/content/ProductCard.jsx
+++ src/pages/shop/content/ProductCard.jsx
@@ -25,7 +25,6 @@ const ProductCard = ({
                     <h2 className="md:text-h3 text-h4 select-none">{title}</h2>
                     <p className="text-gray-6 text-body md:text-base-1 text-ellipsis whitespace-nowrap overflow-hidden">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
                 </div>
-                <p className='text-body text-gray-6'>{desc}</p>
                 <div className="w-full flex justify-between items-center">
                     <Button
                         isPink={true}
