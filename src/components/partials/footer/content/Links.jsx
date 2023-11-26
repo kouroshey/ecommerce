df99287b12c1d8684836728de7f60018ba3a43fa
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 11:57:21 +0330

    little change

--- src/components/partials/footer/content/Links.jsx
+++ src/components/partials/footer/content/Links.jsx
@@ -2,14 +2,14 @@ import ListComp from "./ListComp"
 
 const Links = () => {
   const list = [
-    {id: 1, text: 'خانه', path: '/'},
-    {id: 2, text: 'فروشگاه', path: '/shop'},
-    {id: 3, text: 'بلاگ', path: '/blog'},
-    {id: 4, text: 'ارتباط با ما', path: '/contact'},
+    { id: 1, text: 'خانه', path: '/' },
+    { id: 2, text: 'فروشگاه', path: '/shop' },
+    { id: 3, text: 'بلاگ', path: '/blog' },
+    { id: 4, text: 'درباره ما', path: '/about' },
   ]
   return (
-    <ListComp items={list} title='لینک‌ها' />   
+    <ListComp items={list} title='لینک‌ها' />
   )
 }
 
-export default Links
+export default Links  
