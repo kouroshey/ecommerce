b4cf58ccefdb0df1d5b7a4e7abcda240eb8c4a10
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 13:11:44 +0330

    create and desing spinner

---
+++ src/components/ui/Spinner.jsx
@@ -0,0 +1,11 @@
+import { BeatLoader } from 'react-spinners'
+
+const Spinner = () => {
+    return (
+        <div className='w-screen h-screen flex justify-center items-center'>
+            <BeatLoader color='#EDA3B5' />
+        </div>
+    )
+}
+
+export default Spinner
