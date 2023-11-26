502bee7629742d79b4830fdd7d4274e586dfb758
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 11:56:53 +0330

    add blog section

--- src/components/partials/footer/index.jsx
+++ src/components/partials/footer/index.jsx
@@ -5,18 +5,24 @@ import MoreInfo from "./content/MoreInfo"
 
 import useWidth from "../../../hooks/useWidth"
 import Icons from "./content/Icons"
+import BlogSection from "../../../pages/home/content/blogSection"
+
+import { useLocation } from "react-router-dom"
 
 const Footer = () => {
   const { width, breakPoints } = useWidth()
-
+  const location = useLocation()
   return (
-    <footer className="w-full">
+    <footer className="w-full bg-lightBg">
       {width > breakPoints.sm &&
-        <section className="w-full bg-white py-4">
-          <Navbar width={width} breakPoints={breakPoints} />
-        </section>
+        <div className="bg-light-bg flex flex-col pt-10">
+          {location.pathname === '/' && <BlogSection />}
+          <section className={`${location.pathname === '/' ? 'pt-24 pb-8' : 'py-8'}  w-full bg-white`}>
+            <Navbar width={width} breakPoints={breakPoints} isHeaderNav={false} />
+          </section>
+        </div>
       }
-      <section className="flex px-8 sm:px-12 md:px-12 lg:px-24 py-8 gap-6">
+      <section className="flex px-8 sm:px-12 md:px-12 lg:px-24 py-8 gap-6 bg-white">
         <section className="flex-1 flex md:gap-24 md:justify-start w-full justify-evenly">
           <Links />
           <MoreInfo />
