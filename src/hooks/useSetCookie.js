912da5ae33f674ae01d3468d92b1e02fda217cfa
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-11-26 22:29:19 +0330

    delete

--- src/hooks/useSetCookie.js
+++
@@ -1,8 +0,0 @@
-const useSetCookie = (cname, cvalue, exdays) => {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-export default useSetCookie
