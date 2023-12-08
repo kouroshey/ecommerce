7d5053034011303f1a887b3ce998a61a97889c04
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-12-08 18:31:16 +0330

    create and design RedButton

---
+++ src/components/ui/Button-Red.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react';
+import { Link } from 'react-router-dom'
+import { ButtonPropsType } from './Button-Black';
+
+const RedButton = ({
+    isDisabled,
+    isLink,
+    url,
+    width,
+    textSize,
+    type = 'button',
+    onClick,
+    children,
+}: ButtonPropsType) => {
+    const btnStyles = {
+        defaultClass: `flex items-center justify-center gap-2 px-4 py-2 text-caption md:text-${textSize ? textSize : 'caption'} rounded-md hover:shadow-sm w-${width ? width : 'max'} transition-all`,
+        hover: 'hover:bg-favorite hover:text-white',
+        default: 'border border-favorite text-favorite',
+        disabled: 'bg-gray-2 text-gray-5'
+    }
+
+    const mainStyle = `${btnStyles.defaultClass} ${btnStyles.hover} ${!isDisabled ? btnStyles.default : btnStyles.disabled}`
+
+    return (
+        <>
+            {isLink
+                ? <Link className={mainStyle}
+                    to={url}
+                    onClick={onClick}
+                >
+                    {children}
+                </Link>
+                : < button
+                    className={mainStyle}
+                    type={type}
+                    onClick={onClick}
+                >
+                    {children}
+                </button>
+            }
+        </>
+    )
+}
+
+export default RedButton
