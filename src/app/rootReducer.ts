004a8d41814284e2f04ad9e5d1e8c6af94a6b681
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-12-08 14:21:26 +0330

    add combineReducers and type

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,12 +2,14 @@ import products from '../pages/shop/content/store'
 import layout from './layout'
 import cart from '../pages/cart/content/store'
 import login from '../pages/login/content/store'
+import { combineReducers } from '@reduxjs/toolkit'
 
-const rootReducer = {
+const rootReducer = combineReducers({
     products,
     layout,
     cart,
     login
-}
+})
 
+export type RootReducer = ReturnType<typeof rootReducer>
 export default rootReducer
