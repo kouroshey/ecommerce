09b18c8173eee319c3c411dd2ecb6f006465bc51
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-12-08 14:21:03 +0330

    add type to main store

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { TypedUseSelectorHook } from "react-redux";
 
 export const store = configureStore({
     reducer: rootReducer
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = ReturnType<typeof store.dispatch>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
+
+export type AppSelector = TypedUseSelectorHook<ReturnType<typeof store.getState>>
+export const UseAppSelector: AppSelector = useSelector
+
+export default store
